e7b2bf9071f5da2f6bb672e3c390965c1571c0b7
Tolexy95 <toyin95.olubayo@gmail.com>
2023-07-25 15:32:07 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -13,66 +13,62 @@ const initialState ={
 
 function App() {
   const [state, dispatch] = useReducer(FormReducer, initialState);
-  const [showInput, setShowInput] = useState(1);
-
-  const showNextPage = () => {
-    setShowInput(showInput + 1);
-  }
+  const [step, setStep] = useState(1);
 
   function changeFirstName (e) {
-    dispatch ({type: "CHANGE_FIRST", payload: e.target.value});
+    dispatch({ type: "CHANGE_FIRST", payload: e.target.value });
   }
 
   function changeLastName (e) {
-    dispatch ({type: "CHANGE_LAST", payload: e.target.value});
+    dispatch({ type: "CHANGE_LAST", payload: e.target.value });
   }
 
   function changeEmailAddress (e) {
-    dispatch ({type: "CHANGE_EMAIL", payload: e.target.value});
+    dispatch({ type: "CHANGE_EMAIL", payload: e.target.value });
   }
 
   function changePassword (e) {
-    dispatch ({type: "CHANGE_PASSWORD", payload: e.target.value});
+    dispatch({ type: "CHANGE_PASSWORD", payload: e.target.value });
+  }
+
+  const handleNextStep = () => {
+    setStep(step + 1);
   }
 
   return (
     <div className='App'>
       <form action="">
-        {showInput === 1 && (
-          <div>
-            <label>firstName:</label>
-            <input type="text" onInput={changeFirstName} value={state.firstName} />
-          </div>
+        {step === 1 && (
+          <>
+            <div>
+              <label>firstName:</label>
+              <input type="text" onInput={changeFirstName} value={state.firstName} />
+            </div>
+            <div>
+              <label>LastName:</label>
+              <input type="text" onInput={changeLastName} value={state.lastName} />
+            </div>
+          </>
         )}
 
-        {showInput === 1 && (
-          <div>
-            <label>LastName:</label>
-            <input type="text" onInput={changeLastName} value={state.lastName} />
-          </div>
-        )}
-
-        {showInput === 2 && (
-          <div>
-            <label>Email Address:</label>
-            <input type="email" onInput={changeEmailAddress} value={state.email} />
-          </div>
-        )}
-
-        {showInput === 2 && (
-          <div>
-            <label>Password:</label>
-            <input type="text" onInput={changePassword} value={state.password} />
-          </div>
+        {step === 2 && (
+          <>
+            <div>
+              <label>Email Address:</label>
+              <input type="email" onInput={changeEmailAddress} value={state.email} />
+            </div>
+            <div>
+              <label>Password:</label>
+              <input type="text" onInput={changePassword} value={state.password} />
+            </div>
+          </>
         )}
       </form>
 
-      {showInput === 1 && <button onClick={showNextPage} className='button'>Next</button>}
+      {step === 1 && <button onClick={handleNextStep} className='button'>Next</button>}
+      {step === 2 && <button onClick={handleNextStep} className='button'>Next</button>}
 
-      {showInput === 2 && <button onClick={showNextPage} className='button'>Next</button>}
-
-      {showInput === 3 && (
-        
+      {step === 3 && (
         <div className='pContainer'>
           <p>{state.firstName}</p>
           <p>{state.lastName}</p>
